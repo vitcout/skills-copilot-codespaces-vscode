a04e9811ed9899138ca69cfe59a69c66903b19dd
Vitor Coutinho <74922822+vitcout@users.noreply.github.com>
2025-03-07 20:15:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,81 +1,49 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var port = 8080;
+const express = require('express');
+const app = express();
 
-//create server
-http.createServer(function (req, res) {
-    var url_parts = url.parse(req.url);
-    var path = url_parts.pathname;
-    var query = url_parts.query;
-    console.log(path);
-    console.log(query);
+//read body from request
+app.use(express.json());
 
-    switch (path) {
-        case '/':
-            display_form(res);
-            break;
-        case '/submit':
-            submit_comment(req, res);
-            break;
-        case '/list':
-            list_comments(res);
-            break;
-        default:
-            display_404(path, res);
-            break;
-    }
+//array of comments
+const comments = [
+    {message: 'hello'},
+    {message: 'hi'}
+];
 
-}).listen(port);
+//get request
+app.get('/comments', (req, res) => {
+    res.send(comments);
+});
 
-console.log('Server listening on: http://localhost:%s', port);
+//post request
+app.post('/comments', (req, res) => {
+    const comment = {message: req.body.message};
+    comments.push(comment);
+    res.send(comment);
+});
 
-function display_form(res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<h1>Comments</h1>');
-    res.write('<form action="/submit" method="post">');
-    res.write('<textarea name="comment" rows="5" cols="50"></textarea><br>');
-    res.write('<input type="submit" value="Submit">');
-    res.write('</form>');
-    res.end();
-}
+//server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
-function submit_comment(req, res) {
-    var comment = '';
-    req.on('data', function (data) {
-        comment += data;
-    });
-    req.on('end', function () {
-        fs.appendFile('comments.txt', comment + '\n', function (err) {
-            if (err) throw err;
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.write('Comment submitted. <a href="/list">List comments</a>');
-            res.end();
-        });
-    });
-}
-
-function list_comments(res) {
-    fs.readFile('comments.txt', function (err, data) {
-        if (err) throw err;
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.write('<h1>Comments</h1>');
-        res.write('<pre>' + data + '</pre>');
-        res.write('<a href="/">Add another comment</a>');
-        res.end();
-    });
-}
-
-function display_404(url, res) {
-    res.writeHead(404, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<h1>404 Not Found</h1>');
-    res.write
+//run server using node comments.js
+//use postman to send post request to http://localhost:3000/comments
+//select post method and body -> raw -> json -> {"message": "hello"} -> send
+//get request to http://localhost:3000/comments
+//use postman to send post request to http://localhost:3000/comments
+//select post method and body -> raw -> json -> {"message": "hi"} -> send
+//get request to http://localhost:3000/comments
+//use postman to send post request to http://localhost:3000/comments
+//select post method and body -> raw -> json -> {"message": "how are you"} -> send
+//get request to http://localhost:3000/comments
+//use postman to send post request to http://localhost:3000/comments
+//select post method and body -> raw -> json -> {"message": "good"} -> send
+//get request to http://localhost:3000/comments
+//use postman to send post request to http://localhost:3000/comments
+//select post method and body -> raw -> json -> {"message": "bye"} -> send
+//get request to http://localhost:3000/comments
+//use postman to send post request to http://localhost:3000/comments
+//select post method and body -> raw -> json -> {"message": "goodbye"} -> send
+//get request to http://localhost:3000/comments
